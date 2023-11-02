b4dc1ffe03a9b47075fec667f7f952057de82518
Patrick <patrickfeeneytamayo@gmail.com>
2023-11-02 16:28:37 +0000

    Assign user to image.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
   has_one_attached :picture
-
+  belongs_to :user
 end
---
+++ db/migrate/20231102162408_add_user_id_to_images.rb
@@ -0,0 +1,5 @@
+class AddUserIdToImages < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :images, :user, null: false, foreign_key: true
+  end
+end
